71e58de1483420f4eb2a65f4e2cf7eea72597246
Andrew osmun <andrewosmun@msn.com>
2020-10-31 10:36:42 -0600

    Complete Snake Eyes Challange

--- challenges/SnakeEyes/SnakeEyes.java
+++ challenges/SnakeEyes/SnakeEyes.java
@@ -1,45 +1,48 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeEyes{
     public static void main(String[] args){
-        Random dice = new Random();
-        double dice1 = 0;
-        double dice2 = 0;
         double totalScore = 0;
-        double totalTotalScore = 0;
         double gamesPlayed = 0;
-        double totalGamesPlayed = 100000;
+        
         double numberOfRoles = 0;
         double totalNumberOfRoles = 0;
         int moreThanFourRoles = 0;
-       
-        boolean snakeyes = false;
-        while (gamesPlayed < totalGamesPlayed){
-            snakeyes = false;
-            numberOfRoles = 0;
-            totalScore = 0;
-            while (!snakeyes){
+        Scanner scanner = new Scanner(System.in);
 
-                if (dice1 == 1 && dice2 == 1){
-                    snakeyes = true;
-                }
-                dice1 = dice.nextInt(6);
-                dice2 = dice.nextInt(6);
-                totalScore += dice1 + dice2;
+        System.out.println("How many rounds?");
+        int totalGamesPlayed = scanner.nextInt();
+
+        while (gamesPlayed < totalGamesPlayed){
+            int dice1 = 0;
+            int dice2 = 0;
+            int score = 0;
+            Random dice = new Random();
+            numberOfRoles = 0;
+            score = 0;
+            while (!(dice1 == 1 && dice2 == 1)){
+                
+                dice1 = dice.nextInt(5) + 1;
+                dice2 = dice.nextInt(5) + 1;
+                System.out.println(dice1);
+                System.out.println(dice2);
+                score += dice1 + dice2;
                 numberOfRoles += 1.0;
                 if (numberOfRoles > 4) {
                     moreThanFourRoles += 1;
                 }
             }
-            totalTotalScore += totalScore;
+            
+            totalScore += score;
             totalNumberOfRoles += numberOfRoles;
             gamesPlayed += 1;
         }
         double percentOfMoreThanFour = (moreThanFourRoles / totalGamesPlayed);
         double averageNumberOfRoles = totalNumberOfRoles / totalGamesPlayed;
-        double averageScore = totalTotalScore / totalGamesPlayed;
+        double averageScore = totalScore / totalGamesPlayed;
 
-        System.out.println("The number of roles more than 4 was " + moreThanFourRoles);
+        
         System.out.println("The average number of roles was " + averageNumberOfRoles);
         System.out.println("The average score was " + averageScore);
         System.out.println("The percent of players that had more than 4 roles was " + percentOfMoreThanFour + "%");
